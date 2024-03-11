3bc82cdf71c14aa966f44bdfe57e1e824541ce34
Luca Zaninotto <lucazanny@gmail.com>
2024-03-11 10:02:10 +0100

    better spellcheck && ./spellcheck.sh

--- spellcheck.sh
+++ spellcheck.sh
@@ -1,1 +1,8 @@
-find . -iname [^mod]*.tex -not -path "**/discard*" -not -path "**/old*" -exec aspell -t -c {} \;
+#!/bin/bash
+
+# Find all tex files that do not start as `mod', are not in the
+# `discarded' or `old' folders and run aspell on them
+find . -iwholename "**/src/chapters/**/[^mod]*.tex" \
+     -not -path "**/discard*" \
+     -not -path "**/old*" \
+     -exec xargs aspell -t -c {} \;
